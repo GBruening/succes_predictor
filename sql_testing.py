520f6b9e1176819cf7d2760808e93be312bb7efe
gbruening <garrickbruening@gmail.com>
2021-07-21 15:51:45 -0600

    Fixed small bug with prog_pull finding

--- sql_testing.py
+++ sql_testing.py
@@ -2,6 +2,7 @@
 from sqlalchemy import create_engine
 import pandas as pd
 import json
+import psycopg2
 
 with open('get_guild_list/guild_list_hungering.json', encoding='utf-8') as f:
     guilds = json.load(f)
@@ -16,24 +17,24 @@ with open(guild_info['guild_name']+'_prog_pulls.json', encoding = 'utf-8') as f:
     prog_pulls = json.load(f)
 prog_pulls = pd.read_json(prog_pulls)
 
-engine = create_engine('sqlite://',echo=False)
+# engine = create_engine('sqlite://',echo=False)
 
-prog_pulls.to_sql(name = 'nathria_prog', con = engine)
+# prog_pulls.to_sql(name = 'nathria_prog', con = engine)
 
-# %%
-import psycopg2
-server = 'localhost'
-database = 'dvdrental'
-username = 'postgres'
-password = 'postgres'
+# # %%
+# import psycopg2
+# server = 'localhost'
+# database = 'dvdrental'
+# username = 'postgres'
+# password = 'postgres'
 
-conn = psycopg2.connect('host='+server+' dbname='+database+' user='+username+' password='+password)
-curs = conn.cursor()
-curs.execute("select exists(select * from information_schema.tables where table_name=%s)", ('actor',))
-curs.fetchone()[0]
-# test = curs.execute('SELECT * FROM actor','dvd')
+# conn = psycopg2.connect('host='+server+' dbname='+database+' user='+username+' password='+password)
+# curs = conn.cursor()
+# curs.execute("select exists(select * from information_schema.tables where table_name=%s)", ('actor',))
+# curs.fetchone()[0]
+# # test = curs.execute('SELECT * FROM actor','dvd')
 
-my_table = pd.read_sql('select * from actor', conn)
+# my_table = pd.read_sql('select * from actor', conn)
 
 #%%
 from sqlalchemy import create_engine
