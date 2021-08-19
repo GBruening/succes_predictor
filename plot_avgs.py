8ca924db3fe36db20899a333197214a25beb921a
gbruening <garrickbruening@gmail.com>
2021-08-18 21:00:17 -0600

    Average plotting working

--- plot_avgs.py
+++ plot_avgs.py
@@ -21,9 +21,44 @@ engine = create_engine('postgresql://postgres:postgres@localhost:5432/nathria_pr
 conn = psycopg2.connect('host='+server+' dbname='+database+' user='+username+' password='+password)
 curs = conn.cursor()
 
+curs.execute('select * from "nathria_prog";')
+temp_df = pd.DataFrame(curs.fetchall())
+temp_df.columns = [desc[0] for desc in curs.description]
 
+test = temp_df.groupby(['pull_num','boss_num'], as_index=False).mean()
+# sns.scatterplot(x = 'pull_num', y = 'end_perc', data = test)
 
-# curs.execute('select * from "nathria_prog";')
-# temp_df = pd.DataFrame(curs.fetchall())
-# temp_df.columns = [desc[0] for desc in curs.description]
+g = sns.FacetGrid(test, col = 'boss_num', col_wrap = 4, sharex=False, sharey=True)
+# g.map(sns.scatterplot, 'pull_num','end_perc', color = 'blue')
+g.map(sns.regplot, 'pull_num','end_perc', lowess = True, scatter = False,
+    scatter_kws = {'color': 'black'},
+    line_kws = {'color': 'blue'})
+# g.map(sns.regplot, 'pull_num','end_perc', lowess = True, scatter = False,
+#     scatter_kws = {'color': 'black'},
+#     line_kws = {'color': 'blue'})
+# g.fig.suptitle(guild_info['guild_name']+' Castle Nathria Prog', size = 20)
+
+boss_nums = [5, 3, 2, 6, 1, 10, 8, 9, 4, 7]
+boss_names = [
+    'Shriekwing', \
+    'Huntsman Altimor',
+    'Hungering Destroyer', \
+    "Sun King's Salvation",
+    "Artificer Xy'mox", \
+    'Lady Inerva Darkvein', \
+    'The Council of Blood', \
+    'Sludgefist', \
+    'Stone Legion Generals', \
+    'Sire Denathrius']
+
+# for ax in g.axes.flat:
+#     ax.scatter(x = 'pull_num', y = 'end_perc', lowess = True)
+
+axes = g.axes.flatten()
+for k, ax in enumerate(axes):
+    ax.set_ylabel("Wipe Percent")
+    ax.set_xlabel("Pull Number")
+    ax.set_title(boss_names[k])
+plt.tight_layout()
+
 #%%
