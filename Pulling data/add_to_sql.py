762f0bace22e8e976e35ae556b11492cee0700b5
gbruening <garrickbruening@gmail.com>
2021-09-27 15:45:23 -0600

    Updated the dash a bit to make it look better

--- Pulling data/add_to_sql.py
+++ Pulling data/add_to_sql.py
@@ -11,6 +11,10 @@ from datetime import datetime
 import seaborn as sns
 from matplotlib import pyplot as plt
 
+abspath = os.path.abspath(__file__)
+dname = os.path.dirname(abspath)
+os.chdir(dname)
+
 # %% Define Functions
 
 def is_good_response_json(resp):
@@ -68,7 +72,7 @@ def get_all_logs(guild_info, api_key):
         log_start_ms = single_log['start']
         fight_id = single_log['id']
         log = simple_request(fight_link + fight_id + '?api_key=' + api_key)
-        time.sleep(.5)
+        # time.sleep(.25)
 
         if log:
             log = log.json()
@@ -136,7 +140,8 @@ def get_prog_pulls(df, boss_name):
         first_kill_time = min(kills_df['start_time'])
     else:
         first_kill_time = min(df.query('name == "'+boss_name+'"')['start_time'])
-    return df.query('name == "'+boss_name+'"').query('zoneDifficulty == 5').query('start_time <= '+str(first_kill_time))
+    # return df.query('name == "'+boss_name+'"').query('zoneDifficulty == 5').query('start_time <= '+str(first_kill_time))
+    return df.query('name == "'+boss_name+'"').query('zoneDifficulty == 5')
 
 def add_pull_num(df):
     df = df.sort_values(by = ['start_time'])
@@ -162,7 +167,7 @@ def combine_boss_df(df):
     return only_prog
 
 # Open guild list
-with open('get_guild_list/guild_list_hungering.json', encoding='utf-8') as f:
+with open('../get_guild_list/guild_list_hungering.json', encoding='utf-8') as f:
     guilds = json.load(f)
 
 # %% Setup the SQL Stuff
@@ -181,22 +186,34 @@ temp_df = pd.DataFrame(curs.fetchall())
 temp_df.columns = [desc[0] for desc in curs.description]
 np.unique(temp_df['guild_name'])
 
-curs.execute('select distinct guild_name from nathria_prog')
-already_added_guilds = [item[0] for item in curs.fetchall()]
-already_added_length = len(already_added_guilds)
+curs.execute("select exists(select * from information_schema.tables where table_name=%s)",\
+    ('nathria_prog_allpulls',))
+if curs.fetchone()[0]:
+    # curs.execute("select * from nathria_prog_allpulls where guild_name = '"+str(guild_name)+"'")
+    curs.execute('select distinct guild_name from nathria_prog_allpulls')
+    already_added_guilds = [item[0] for item in curs.fetchall()]
+    already_added_length = len(already_added_guilds)
+else:
+    already_added_guilds = []
+    already_added_length = 0
+    # if len(already_added_guilds) == 1:
+        # print('Guild: '+str(guild_name)+' already in SQL table. Continuing...')
+        # return None     
+# curs.execute('select distinct guild_name from nathria_prog_allpulls')
+# already_added_guilds = [item[0] for item in curs.fetchall()]
+# already_added_length = len(already_added_guilds)
 # %% Get new data.
 # DC is guild 725
 # for guild_num in np.arange(len(guilds)):
 # guild_num = 1075
-for guild_num in np.arange(1075,len(guilds)):
+for guild_num in np.arange(1,len(guilds)):
     guild_info = {'guild_name': guilds[guild_num]['name'],
                 'realm': guilds[guild_num]['realm'].replace(' ','-').replace("'",''),
                 'region': guilds[guild_num]['region']}
     if not guild_info['guild_name'] in already_added_guilds:
         print('Pulling data from '+guild_info['guild_name']+'. Number '+str(guild_num)+'.')
-        with open('..//Warcraftlogs//api_key.txt.') as f:
+        with open('..//..//Warcraftlogs//api_key.txt.') as f:
             api_key = f.readlines()[0]
-
         try:
             pulls = get_all_logs(guild_info = guild_info, api_key = api_key)
         except:
@@ -209,13 +226,16 @@ for guild_num in np.arange(1075,len(guilds)):
 
             prog_pulls = combine_boss_df(pulls.copy(deep = True))
             prog_pulls['guild_name'] = guild_info['guild_name']
+            prog_pulls['realm'] = guild_info['realm']
+            prog_pulls['region'] = guild_info['region']
+            prog_pulls['guild_num'] = guild_num
 
             # if not guild_info['guild_name'] in np.unique(already_added_guilds):
             print('Adding guild '+guild_info['guild_name']+' to nathria_prog postgressql table.')
-            prog_pulls.to_sql('nathria_prog', engine, if_exists='append')
+            prog_pulls.to_sql('nathria_prog_allpulls', engine, if_exists='append')
 
-            curs.execute('select distinct guild_name from nathria_prog')
-            pull_length = len([item[0] for item in curs.fetchall()])
+            # curs.execute('select distinct guild_name from nathria_prog')
+            # pull_length = len([item[0] for item in curs.fetchall()])
             # if already_added_length == pull_length:
             #     break
             # else:
--- dash2/test.py
+++ dash2/test.py
@@ -4,6 +4,7 @@
 # visit http://127.0.0.1:8050/ in your web browser.
 #%%
 import dash
+import dash_bootstrap_components as dbc
 from dash import Dash
 import dash_core_components as dcc
 import dash_html_components as html
@@ -13,7 +14,8 @@ from dash.dependencies import Input, Output
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
-app = Dash(__name__, external_stylesheets=external_stylesheets)
+# app = Dash(__name__, external_stylesheets=external_stylesheets)
+app = dash.Dash(external_stylesheets=[dbc.themes.DARKLY])
 
 #%% Create Data
 import numpy as np
@@ -38,26 +40,34 @@ curs = conn.cursor()
 curs.execute('select distinct guild_name from nathria_prog')
 guilds = [item[0] for item in curs.fetchall()]
 
+
+if 'conn' in locals():
+    conn.close()
+engine = create_engine('postgresql://postgres:postgres@localhost:5432/nathria_prog')
+conn = psycopg2.connect('host='+server+' dbname='+database+' user='+username+' password='+password)
+curs = conn.cursor()
+
 #%%
 @app.callback(
     Output('single_guild_graph', 'figure'),
-    Input('guild_name', 'value')
+    Input('guild_name', 'value'),
+    Input('prog_or_all', 'value')
 )
-def update_fig(guild_name):
+def update_fig(guild_name, prog_or_all):
         
-    if 'conn' in locals():
-        conn.close()
-    engine = create_engine('postgresql://postgres:postgres@localhost:5432/nathria_prog')
-    conn = psycopg2.connect('host='+server+' dbname='+database+' user='+username+' password='+password)
-    curs = conn.cursor()
-    curs.execute('select distinct guild_name from nathria_prog')
-    guilds = [item[0] for item in curs.fetchall()]
+    # curs.execute('select distinct guild_name from nathria_prog')
+    # guilds = [item[0] for item in curs.fetchall()]
 
-    curs.execute("select * from nathria_prog where guild_name = '" + str(guild_name)+"'")
+    if prog_or_all == 'prog_only_pulls':
+        curs.execute("select * from nathria_prog where guild_name = '" + str(guild_name)+"'")
+    elif prog_or_all == 'all_pulls':
+        curs.execute("select * from nathria_prog_allpulls where guild_name = '" + str(guild_name)+"'")
+
     # curs.execute("select * from nathria_prog where guild_name = '" + str(guilds[int(guild_num)])+"'")
     pulls = pd.DataFrame(curs.fetchall())
     pulls.columns = [desc[0] for desc in curs.description]
     newdf = pulls.sort_values(by = 'boss_num')
+    n_bosses = len(np.unique(pulls['boss_num']))
     # # Fixing Data frames cause I don't know why
     # newdf = pd.DataFrame()
     # for k in np.unique(pulls['boss_num']):
@@ -92,14 +102,17 @@ def update_fig(guild_name):
     fig.for_each_yaxis(lambda yaxis: yaxis.update(showticklabels=True))
     fig.for_each_annotation(lambda a: a.update(text=a.text.split("=")[-1]))
 
+
     fig.update_layout(
         template = 'plotly_dark',
-        autosize=False,
-        width=1500,
-        height=1500,
+        plot_bgcolor = 'rgba(34,34,34,255)',
+        paper_bgcolor = 'rgba(34,34,34,255)',
+        autosize=True,
+        # width=1500,
+        height=np.ceil(n_bosses/2)*300,
         margin=dict(
-            l=70,
-            r=70,
+            l=150,
+            r=150,
             b=30,
             t=30,
             pad=4
@@ -108,47 +121,46 @@ def update_fig(guild_name):
     )
     return fig
 
-@app.callback(
-    Output(component_id='my-output', component_property='children'),
-    Input(component_id='guild_name', component_property='value')
-)
-def update_output_div(guild_name):
-    if 'conn' in locals():
-        conn.close()
-    engine = create_engine('postgresql://postgres:postgres@localhost:5432/nathria_prog')
-    conn = psycopg2.connect('host='+server+' dbname='+database+' user='+username+' password='+password)
-    curs = conn.cursor()
+# @app.callback(
+#     Output(component_id='my-output', component_property='children'),
+#     Input(component_id='guild_name', component_property='value')
+# )
+# def update_output_div(guild_name):
+#     if 'conn' in locals():
+#         conn.close()
+#     engine = create_engine('postgresql://postgres:postgres@localhost:5432/nathria_prog')
+#     conn = psycopg2.connect('host='+server+' dbname='+database+' user='+username+' password='+password)
+#     curs = conn.cursor()
 
-    curs.execute('select distinct guild_name from nathria_prog')
-    guilds = [item[0] for item in curs.fetchall()]
-    # return 'Output: {}'.format(str(guilds[int(guild_num)]))
-    return 'Output: {}'.format(str(guild_name))
+#     curs.execute('select distinct guild_name from nathria_prog')
+#     guilds = [item[0] for item in curs.fetchall()]
+#     # return 'Output: {}'.format(str(guilds[int(guild_num)]))
+#     return 'Output: {}'.format(str(guild_name))
+
 #%% Make App
 app.layout = html.Div(children=[
-    html.H1('Plot 1'),
-    html.H6("Change the value in the text box to see callbacks in action!"),
-    # html.Div([
-    #     "Input: ",
-    #     # dcc.Input(id='guild_num', placeholder = 'Enter a guild number...', value='2748', type='text')
-    #     dcc.Textarea(id = 'guild_name',
-    #                  placeholder = 'Enter a guild name...',
-    #                  value = 'Dinosaur Cowboys',
-    #                  style = {'width': '100'})
-    # ]),
+    html.H1('Castle Nathria Pull Data'),
     html.Div([
-        "Input2: ",
-        # dcc.Input(id='guild_num', placeholder = 'Enter a guild number...', value='2748', type='text')
+        "Choose guild: ",
         dcc.Dropdown(id = 'guild_name',
-                     options = [{'label': name, 'value': name} for k, name in enumerate(guilds)],
-                     value = 'Dinosaur Cowboys')
+                     options = [{'label': name, 'value': name} for k, name in enumerate(sorted(guilds))],
+                     value = 'Dinosaur Cowboys',
+                     style = {'color': 'black',
+                              'width': '300px'})
+    ]),
+    html.Div([
+       "All pulls or Progression Pulls?",
+        dcc.RadioItems(options=[
+            {'label': 'All Pulls', 'value': 'all_pulls'},
+            {'label': 'Progression Only', 'value': 'prog_only_pulls'}
+        ],
+        id = 'prog_or_all',
+        value = 'prog_only_pulls',
+        labelStyle={'display': 'flex'}
+        )
     ]),
     html.Br(),
-    html.Div(id='my-output'),
-
-    html.Div(children='''
-        Testing plotting with dash.
-    '''),
-
+    # html.Div(id='my-output'),
     dcc.Graph(
         id='single_guild_graph'
     )
